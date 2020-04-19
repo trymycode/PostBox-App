28bb7bcca9e797c47dea613049ef91e424d7f708
trymycode <imitasingha1994@gmail.com>
2020-04-19 16:26:40 +0530

    Added redux,react-redux and set up the store

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
+          <i className="fa fa-archive big" aria-hidden="true"></i>
           PostBox
         </Link>
         {/* show these two links based on user authentication status */}
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 const SignedInLinks = () => {
   return (
     <ul className="right">
-        <li><NavLink to="/">New Project</NavLink></li>
+        <li><NavLink to="/create-post">Create New Post</NavLink></li>
         <li><NavLink to="/">Logout</NavLink></li>
-        {/* show the name who has loged in */}
+        {/* show the shortname of who has logged in */}
         <li><NavLink to="/" className="btn btn-floating skyblue lighten-1">PB</NavLink></li>
     </ul>
   );
