9b0d6d505e15ead425bd4da630ef123e1820002d
trymycode <imitasingha1994@gmail.com>
2020-04-19 22:52:30 +0530

    installed firebase npm package and created firebase config file

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+var firebaseConfig = {
+    apiKey: "AIzaSyBPZAqChmaudLsTC2QHL0zsQX0OhUrkWQ4",
+    authDomain: "social-media-app-postbox.firebaseapp.com",
+    databaseURL: "https://social-media-app-postbox.firebaseio.com",
+    projectId: "social-media-app-postbox",
+    storageBucket: "social-media-app-postbox.appspot.com",
+    messagingSenderId: "164712208613",
+    appId: "1:164712208613:web:8551a5d490079f27779c0c",
+    measurementId: "G-VKN24J9ZC3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore.settings({ timestampsInSnapshots:true });
+  export default firebase;
