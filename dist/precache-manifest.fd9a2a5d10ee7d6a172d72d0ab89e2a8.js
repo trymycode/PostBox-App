68c545d2a45d8044ca4b4d9d7439e7be2c263062
trymycode <imitasingha1994@gmail.com>
2020-04-24 20:48:36 +0530

    Run build command and deplyed in firebase

---
+++ dist/precache-manifest.fd9a2a5d10ee7d6a172d72d0ab89e2a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f8318d5d06c22714411c4a2d5b16fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920100dc8c14e9977695",
+    "url": "/static/css/main.3fd01d67.chunk.css"
+  },
+  {
+    "revision": "3bd413517686a1aed1f5",
+    "url": "/static/js/2.ec99b1b4.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.ec99b1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920100dc8c14e9977695",
+    "url": "/static/js/main.2595eecc.chunk.js"
+  },
+  {
+    "revision": "4dba0b116334145f14bd",
+    "url": "/static/js/runtime-main.7b18f848.js"
+  }
+]);
